21523a0c898957d42c8898baddd32fa05cb3c9c0
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-06 07:00:29 +0000

    各ページの雛形を作成した

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      ログイン画面
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/components/UserInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserInfo = () => {
+  return (
+    <div>
+      ユーザ情報ページ
+    </div>
+  )
+}
+
+export default UserInfo
