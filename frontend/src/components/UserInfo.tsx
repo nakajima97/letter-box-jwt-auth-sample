5964aa048d68fd1a69776e5378e291990b6de92e
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-07 06:03:55 +0000

    ログイン状態を判断するためのページを作成

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const UserInfo = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/user/show/1")
+      .then((res) => setMessage(res.data.name))
+      .catch((error) => console.log(error))
+  })
+
   return (
     <div>
-      ユーザ情報ページ
+      {message}
     </div>
   )
 }
