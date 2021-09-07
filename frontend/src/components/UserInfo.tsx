b19d72be9b72af1faced4dc98e407a5c44a2f75c
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-07 07:29:52 +0000

    jwtをサーバに送るコード作成

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-
-const options = {
-  headers: {
-    'content-type': 'application/json'
-  },
-  withCredentials: true
-}
+import { useCookies } from 'react-cookie'
 
 const UserInfo = () => {
   const [message, setMessage] = useState("");
+    // eslint-disable-next-line
+  const [cookies, setCookie, removeCookie] = useCookies(["jwt"])
 
   useEffect(() => {
+    const options = {
+      headers: {
+        'content-type': 'application/json',
+        'authorization': cookies.jwt
+      },
+      withCredentials: true,
+    }
+
     axios.get("http://localhost:3000/user/show/1", options)
       .then((res) => setMessage(res.data.name))
       .catch((error) => console.log(error))
